d1c30fce9f5696f24b77ec5d2c8ddba557587035
KimHgYg <guddydcjswo1@gmail.com>
2018-11-06 03:47:09 +0000

    mongodb connection established trying using db with module

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,46 +2,54 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var session = require("express-session");
-var mongoose = require('mongoose');
-var tunnel = require("tunnel-ssh");
 var fs = require("fs");
 
 app.set('views',__dirname + '/views');
 app.set('view engine','ejs');
 app.engine('html',require('ejs').renderFile);
 
+//db-connect mudule
+module.exports = function(mongoose){
+    var tunnel = require("tunnel-ssh");
+    
+    //mongodb configure
+    var config = {
+        username:'ubuntu',
+        host:'52.79.249.174',
+        port:22,
+        agent:process.env.SSH_AUTH_SOCK,
+        privatekey:fs.readFileSync('/home/bitnami/.ssh/id_rsa'),
+        dstHost:'127.0.0.1',
+        dstPort:27017,
+        localHost:'127.0.0.1',
+        localPort:27017,
+        keepAlive:true
+    };
+    var db = mongoose.connection;
+    
+    var db_server = tunnel(config, function(error, server){
+        console.log('Tunnel open');
+        if(error) console.error(error);
+        
+        
+        db.on('error',console.error.bind(console, 'DB connection error:'));
+        db.once('open',function(){
+            console.log("DB connection established");
+        });
+        mongoose.connect('mongodb://52.79.249.174:27017/crawling',{
+            useNewUrlParser:true
+        });
+    });
+};
+
 //port connect and listen
 var port = process.env.PORT || 8080;
-
 var server = app.listen(port, function() {
     console.log("Express server has satrted on port "+port);
 });
 
-//mongodb configure
-var config = {
-    username:'ubuntu',
-    host:'52.79.249.174',
-    port:22,
-    agent:process.env.SSH_AUTH_SOCK,
-    privatekey:require('fs').readFileSync('/home/bitnami/.ssh/id_rsa'),
-
-    dstPort:27017
-};
-
-var db_server = tunnel(config, function(error, server){
-    if(error) console.error(error);
-    mongoose.connect('mongodb://localhost:27017/crawling',{useNewUrlParser:true});
-    
-    var db = mongoose.connection;
-    db.on('error',console.error.bind(console, 'DB connection error:'));
-    db.once('open',function(){
-        console.log("DB connection established");
-    });
-});
-
 //express configure
 app.use(express.static('public'));
-
 
 //body-parser configure
 app.use(bodyParser.json());
